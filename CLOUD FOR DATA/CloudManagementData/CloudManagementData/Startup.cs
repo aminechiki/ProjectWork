7e01a04a4ddbc43e14fcd27f5d10a31d7c4d7f1b
aminechiki <aminechiki55@gmail.com>
2023-05-30 18:01:14 +0200

    complettao pirma versione della azure funtion, impementato dependency injection

---
+++ CLOUD FOR DATA/CloudManagementData/CloudManagementData/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using ApplicationCore;
+using Data;
+
+
+[assembly: FunctionsStartup(typeof(CloudManagementData.Startup))]
+
+namespace CloudManagementData
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddRoomData();
+            builder.Services.AddApplicationCore();
+        }
+    }
+}
---
+++ CLOUD FOR DATA/CloudManagementData/Data/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using ApplicationCore.Interfaces;
+using Data.Repository;
+
+
+namespace Data
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddRoomData(this IServiceCollection services)
+        {
+            services.AddSingleton<IStorageRoomData, StorageRoomData>();
+            return services;
+        }
+    }
+}

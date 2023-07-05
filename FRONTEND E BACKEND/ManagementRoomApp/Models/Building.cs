de16e8c99e2708967e7ba7201c99ebf5ceba6439
ITSCOR1908\Administrator <luca.pagnucco@stud.itsaltoadriatico.it>
2023-07-05 12:26:56 +0200

    commit per merge # 2

--- FRONTEND E BACKEND/ManagementRoomApp/Models/Building.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManagementRoomApp.Models
-{
-    public class Building
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int IdDevice { get; set; }
-    }
-}
